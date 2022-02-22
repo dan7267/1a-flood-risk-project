db702b1aba4a00a17632165ff7d213890566f2af
Sam-Fitzgerald <slf62@cam.ac.uk>
2022-02-22 16:39:18 +0000

    Task 2B

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,7 +1,19 @@
 from .stationdata import build_station_list
 from .stationdata import update_water_levels
 from .station import MonitoringStation
+
 def stations_level_over_threshold(stations, tol):
+    
+    floods =[]
+    stations = build_station_list()
+    update_water_levels(stations)
+    for station in stations:
+        if station.typical_range_consistent() == True and station.relative_water_level() > tol:
+            floods.append((station.name,station.relative_water_level()))
+    floods = sorted(floods, key=lambda tup: tup[1], reverse = True)
+    return floods
+
+'''def stations_level_over_threshold(stations, tol):
     floods = []
     sortedfloods = []
     stations = build_station_list()
@@ -11,13 +23,13 @@ def stations_level_over_threshold(stations, tol):
         if station.typical_range_consistent() == True:
             if station.relative_water_level() > tol:
                 print(station.name)
-                '''floods.append((station.name, station.relative_water_level))
-    sortedfloods = sorted(floods, key=lambda tup: tup[1])'''
+                floods.append((station.name, station.relative_water_level))
+    sortedfloods = sorted(floods, key=lambda tup: tup[1])
     print(floods)
     return sortedfloods
 
-'''stations_level_over_threshold(MonitoringStation, 0.8)'''
-'''stations = build_station_list()
+stations_level_over_threshold(MonitoringStation, 0.8)
+stations = build_station_list()
     update_water_levels(stations)
     for station in stations:
         if station.typical_range_consistent() == True and station.relative_water_level() > 0.8:
